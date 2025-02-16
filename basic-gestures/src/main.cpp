58c6a949ff52f96c1474aecb6267dba636f1c54e
Edu Meneses <eduardo@edumeneses.com>
2025-02-16 13:27:10 -0500

    add IMU simulator

--- basic-gestures/src/main.cpp
+++ basic-gestures/src/main.cpp
@@ -5,6 +5,7 @@
 //****************************************************************************//
 
 #include "Arduino.h"
+#include <iostream>
 
 // Include Puara's module manager
 // If using Arduino.h, include it before including puara.h
@@ -14,18 +15,22 @@
 // https://github.com/Puara/puara-gestures
 #include "puara/gestures.h"
 
-#include <iostream>
+// Include an IMU simulator to generate some data
+#include "imu_simulator.h"
 
-// Initialize Puara's module manager
+// Instatiate Puara's module manager
 Puara puara;
 
-// Dummy sensor data
-float sensor;
+// Instatiate UMI simulator
+IMUSimulator imu;
 
 void setup() {
     #ifdef Arduino_h
         Serial.begin(115200);
     #endif
+
+    // Initialize the IMU simulator
+    imu.begin(); 
 
     /*
      * the Puara start function initializes the spiffs, reads config and custom json
@@ -40,7 +45,7 @@ void setup() {
      */
     std::cout << "\n" 
     << "Settings stored in data/settings.json:\n" 
-    << "Hitchhiker: "           << puara.getVarText ("Hitchhiker")            << "\n"
+    << "Hitchhiker: "           << puara.getVarText  ("Hitchhiker")           << "\n"
     << "answer_to_everything: " << puara.getVarNumber("answer_to_everything") << "\n"
     << "variable3: "            << puara.getVarNumber("variable3")            << "\n"
     << std::endl;
@@ -48,17 +53,25 @@ void setup() {
 
 void loop() {
 
-    // Update the dummy sensor variable with random number
-    sensor = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10));
+    // Update the IMU simulated data
+    imu.update(); 
 
-    // print the dummy sensor data
+    // print the simulated IMU data
     std::cout << "\n" 
-    << "Settings stored in data/settings.json:\n" 
-    << "Dummy sensor value: " << sensor << "\n"
+    << "Smilated IMU data:\n [" 
+    << imu.getAccelX() << ","
+    << imu.getAccelY() << ","
+    << imu.getAccelZ() << ","
+    << imu.getGyroX()  << ","
+    << imu.getGyroY()  << ","
+    << imu.getGyroZ()  << ","
+    << imu.getMagX()   << ","
+    << imu.getMagY()   << ","
+    << imu.getMagZ()   << "]"
     << std::endl;
 
-    // run at 1 Hz (1 message per second)
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // run at ~100 Hz
+    vTaskDelay(10 / portTICK_PERIOD_MS);
 }
 
 /* 
