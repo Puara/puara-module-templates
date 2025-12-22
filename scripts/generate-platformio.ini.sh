4e2d969f0b2c58d871500d507153108ee1cd7e32
Charles B <bicari.charles@gmail.com>
2025-12-22 14:29:35 -0500

    testing CI with JM new logging message structure

--- scripts/generate-platformio.ini.sh
+++ scripts/generate-platformio.ini.sh
@@ -28,7 +28,7 @@ EOL
 # Add dependencies based on the template
 case "${TEMPLATE}" in
   basic)
-    echo "    https://github.com/Puara/puara-module.git" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-module.git#ccfaed15c571d5e355dc82eb83be9ffea98cb651" >> "${OUTPUT_FILE}"
     ;;
   basic-osc)
     echo "    https://github.com/Puara/puara-module.git#6297c8e5b00302843ca7539bc246226ff03e6ae0" >> "${OUTPUT_FILE}"
@@ -37,7 +37,7 @@ case "${TEMPLATE}" in
 # add basic-gestures
   ble-advertising)
     echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"
-    echo "    https://github.com/Puara/puara-module.git#835ecd59b7e58318b174a5346d9d535be2cb6a8f" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-module.git#ccfaed15c571d5e355dc82eb83be9ffea98cb651" >> "${OUTPUT_FILE}"
     echo "    arduino-libraries/ArduinoBLE" >> "${OUTPUT_FILE}"
     echo "    johboh/nlohmann-json@3.11.3" >> "${OUTPUT_FILE}"
     ;;
