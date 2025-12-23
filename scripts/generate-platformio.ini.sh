9570cc3d30c2cc74d17651ab35508635fd80b811
Charles B <bicari.charles@gmail.com>
2025-12-23 12:09:43 -0500

    added tests for littlefs/spiffs variations and two other templates

--- scripts/generate-platformio.ini.sh
+++ scripts/generate-platformio.ini.sh
@@ -8,6 +8,16 @@ EXTRA_FLAGS=$3
 # We'll put the temporary platformio.ini file in the current template folder
 OUTPUT_FILE="${TEMPLATE}/platformioTemp.ini"
 
+# Determine if -DPUARA_SPIFFS should be added based on the template
+SPIFFS_FLAG=""
+case "${TEMPLATE}" in
+  basic-spiffs|basic-gestures-spiffs|ble-advertising-spiffs)
+    SPIFFS_FLAG="-DPUARA_SPIFFS"
+    ;;
+esac
+
+
+
 # Write the file
 cat <<EOL > "${OUTPUT_FILE}"
 [platformio]
@@ -19,7 +29,7 @@ board_build.partitions = min_spiffs_no_OTA.csv
 monitor_speed = 115200
 monitor_echo = yes
 monitor_filters = default,esp32_exception_decoder
-build_flags = -std=gnu++2a ${EXTRA_FLAGS}
+build_flags = -std=gnu++2a ${EXTRA_FLAGS} ${SPIFFS_FLAG}
 build_unflags = -std=gnu++11 -std=gnu++14 -std=gnu++17
 lib_deps =
 EOL
@@ -27,19 +37,46 @@ EOL
 
 # Add dependencies based on the template
 case "${TEMPLATE}" in
-  basic)
+  basic-littlefs)
     echo "    https://github.com/Puara/puara-module.git" >> "${OUTPUT_FILE}"
+    echo "board_build.filesystem = littlefs" >> "${OUTPUT_FILE}"
+    ;;
+  basic-spiffs)
+    echo "    https://github.com/Puara/puara-module.git" >> "${OUTPUT_FILE}"
+    echo "board_build.filesystem = spiffs" >> "${OUTPUT_FILE}"
+    ;;
+  basic-gestures-littlefs)
+    echo "    https://github.com/Puara/puara-module.git" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"    
+    echo "board_build.filesystem = littlefs" >> "${OUTPUT_FILE}"
+    ;;
+  basic-gestures-spiffs)
+    echo "    https://github.com/Puara/puara-module.git" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"        
+    echo "board_build.filesystem = spiffs" >> "${OUTPUT_FILE}"
     ;;
   basic-osc)
     echo "    https://github.com/Puara/puara-module.git#6297c8e5b00302843ca7539bc246226ff03e6ae0" >> "${OUTPUT_FILE}"
     echo "    https://github.com/cnmat/OSC#3.5.8" >> "${OUTPUT_FILE}"
     ;;
-# add basic-gestures
-  ble-advertising)
+  ble-advertising-littlefs)
     echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"
     echo "    https://github.com/Puara/puara-module.git#835ecd59b7e58318b174a5346d9d535be2cb6a8f" >> "${OUTPUT_FILE}"
     echo "    arduino-libraries/ArduinoBLE" >> "${OUTPUT_FILE}"
     echo "    johboh/nlohmann-json@3.11.3" >> "${OUTPUT_FILE}"
+    echo "board_build.filesystem = littlefs" >> "${OUTPUT_FILE}"
+    ;;
+  ble-advertising-spiffs)
+    echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-module.git#835ecd59b7e58318b174a5346d9d535be2cb6a8f" >> "${OUTPUT_FILE}"
+    echo "    arduino-libraries/ArduinoBLE" >> "${OUTPUT_FILE}"
+    echo "    johboh/nlohmann-json@3.11.3" >> "${OUTPUT_FILE}"
+    echo "board_build.filesystem = spiffs" >> "${OUTPUT_FILE}"
+    ;;
+  button-osc)
+    echo "    https://github.com/Puara/puara-module.git#835ecd59b7e58318b174a5346d9d535be2cb6a8f" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/cnmat/OSC#3.5.8" >> "${OUTPUT_FILE}"
+    echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"    
     ;;
   libmapper-osc)
     echo "    https://github.com/Puara/puara-gestures.git" >> "${OUTPUT_FILE}"
