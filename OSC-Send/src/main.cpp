d2c0334bc9859a395fed779456f3928ada833dda
Charles B <bicari.charles@gmail.com>
2026-01-22 11:35:54 -0500

    working on OSC-Send template

--- OSC-Send/src/main.cpp
+++ OSC-Send/src/main.cpp
@@ -15,10 +15,7 @@
 
 #include <iostream>
 
-// Initialize Puara's module manager
 Puara puara;
-
-// UDP instances to let us send and receive packets
 WiFiUDP Udp;
 
 std::string oscIP_1{};
@@ -37,13 +34,16 @@ void setup() {
         Serial.begin(115200);
     #endif
 
-/* puara.start() initializes the filesystem with given configurations and settings.
- * The process then tries to connect to the WiFi Network (SSID) defined in config.json. 
- * User may change default SSID values. If process cannot find SSID, it will create it's
- * own WiFi Access Point (AP) to which user may connect and recieve it's OSC messages.
- * The process will also start a webserver where user may modify configurations and 
- * settings using any browser. Finally, the process instantiates serial listening, 
- * MDNS service, and scans for surrounding WiFi networks.
+/* 
+ puara.start() initializes the filesystem with given configurations and settings.
+ The process then tries to connect to the WiFi Network (SSID) defined in config.json. 
+ User may change default SSID values. If process cannot find SSID, it will create 
+ it's own WiFi Access Point (AP) to which user may connect and receive it's OSC 
+ messages. 
+ The process will also start a webserver where user may modify configurations and 
+ settings using any browser while being connected to AP. 
+ Finally, the process instantiates serial listening, MDNS service, and scans for 
+ surrounding WiFi networks.
  */
     puara.start();
     Udp.begin(puara.getVarNumber("localPORT"));
@@ -51,42 +51,49 @@ void setup() {
     // This allows us to reconfigure the UDP reception port
     puara.set_settings_changed_handler(onSettingsChanged);
 
-    // If needed, define your pins here. Refer to your board's documentation for appropriate 
-    // pin numbers. These numbers are given as placeholder examples.
+/*
+ If needed, define your pins here. Refer to your board's documentation for 
+ appropriate pin numbers. The numbers given here are only placeholders.
+*/
+    /* Setting pin 7 as an input. */
+    // pinMode(7, INPUT);
 
-    // Setting a pin as input
-    // pinMode(7, INPUT); // Sets pin 7 as an input pin
-
-    // Setting a pin as an input with pull-up resistor
-    // pinMode(2, INPUT_PULLUP); // Sets pin 2 as an input pin with an internal pull-up resistor enabled
+    /* Setting pin 2 as an input with internal pull-up resistor.    */
+    /* Default value of pin is HIGH when not connected to ground.   */
+    // pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
 
-    // Retrieve OSC settings from Puara. 
-    // These can be changed in the web interface and will automatically update here.
+/* 
+ Retrieve OSC IP and PORT from stored data. User may modify IP/PORT 
+ values in webpage and these changes will update automatically here.
+*/    
     oscIP_1 = puara.getVarText("oscIP");
     oscPort_1 = puara.getVarNumber("oscPORT");
 
-    // Update the dummy sensor variable with a random number
+/* Update the dummy sensor variable with a random number between 0 and 10 */
     sensor = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10));
 
-    // If using actual sensors, read their values here instead of the dummy data.
-    // Example for reading an analog sensor connected to pin A0
-    // sensor = analogRead(A0); // Reads the value from the analog pin A0
+/* If using actual sensors, read their values here instead of the dummy data.
+    // Example for reading an analog sensor connected to pin 7
+    // sensor = analogRead(7);
 
+    // Example for reading a digital signal (LOW/HIGH) connected to pin 2
+    // sensor = digitalRead(2);
 
-    // print the dummy sensor data
+    // Print the dummy sensor data
     Serial.print("Dummy sensor value: ");
     Serial.println(sensor);
 
     /*
-     * Sending OSC messages.
-     * If you're not planning to send messages to both addresses (OSC1 and OSC2),
-     * it is recommended to set the address to 0.0.0.0 to avoid cluttering the
-     * network (WiFiUdp will print a warning message in those cases).
+     * Sending OSC messages.  
+     * This sends the sensor value to the defined OSC IP and port.
      */
-    if (!oscIP_1.empty() && oscIP_1 != "0.0.0.0") { // set namespace and send OSC message for address 1
+    if (!oscIP_1.empty() && oscIP_1 != "0.0.0.0") {
+    // OSCmessage uses puara.dmi_name() which is the "device" and "id" fields from config.json file. 
+    // User may define these fields and rebuild filesystem to change the OSC address name. 
+    // Default OSC address name is "Puara_001".
         OSCMessage msg1(("/" + puara.dmi_name()).c_str());
         msg1.add(sensor);
         Udp.beginPacket(oscIP_1.c_str(), oscPort_1);
@@ -94,22 +101,6 @@ void loop() {
         Udp.endPacket();
         msg1.empty();
         std::cout << "Message send to " << oscIP_1 << ":" << oscPort_1 << std::endl;
-    }
-
-    /*
-     * Receiving OSC messages. This expects a float value on the OSC address /hi/there.
-     */
-    OSCMessage inmsg;
-    int size = Udp.parsePacket();
-    while (size--) {
-        inmsg.fill(Udp.read());
-    }
-    if (!inmsg.hasError()) {
-        if (inmsg.fullMatch("/hi/there") && inmsg.isFloat(0)) {
-            Serial.print("got a float on address /hi/there : ");
-            // you could receive more than one value in a message by checking inmsg.isFloat(1) or more.
-            Serial.println(inmsg.getFloat(0));
-        }
     }
 
     // run at 1 Hz (1 message per second)
