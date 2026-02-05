dd54845f89cf4b8a45c35e2ba35025cd14a53a71
Charles B <bicari.charles@gmail.com>
2026-02-05 16:18:30 -0500

    copied basic template as base for ftm development and testing

---
+++ ftm/src/main.cpp
@@ -0,0 +1,77 @@
+//****************************************************************************//
+// Puara Module Manager                                                       //
+// Société des Arts Technologiques (SAT)                                      //
+// Input Devices and Music Interaction Laboratory (IDMIL), McGill University  //
+//****************************************************************************//
+
+#include "Arduino.h"
+
+// Include Puara's module manager
+// If using Arduino.h, include it before including puara.h
+#include "puara.h"
+
+#include <iostream>
+
+// Initialize Puara's module manager
+Puara puara;
+
+// Dummy sensor data
+float sensor;
+
+void setup() {
+    #ifdef Arduino_h
+        Serial.begin(115200);
+    #endif
+
+    /*
+     * The Puara start function initializes the spiffs, reads the config and custom JSON
+     * settings, starts the wi-fi AP, connects to SSID, starts the webserver, serial
+     * listening, MDNS service, and scans for WiFi networks.
+     *
+     * The first parameter governs which method puara will use to read serial command, the default beign UART.
+     *
+     * The second parameter governs which debug log level should be printed. The default is to
+     * print warnings and errors but you can print more.
+     */
+    puara.start(PuaraAPI::UART_MONITOR, ESP_LOG_VERBOSE);
+    /*
+     * Printing custom settings stored. The data/config.json values will print during
+     * Initialization (puara.start)
+     */
+    std::cout << "\n"
+    << "Settings stored in data/settings.json:\n"
+    << "Hitchhiker: "           << puara.getVarText ("Hitchhiker")            << "\n"
+    << "answer_to_everything: " << puara.getVarNumber("answer_to_everything") << "\n"
+    << "variable3: "            << puara.getVarNumber("variable3")            << "\n"
+    << std::endl;
+}
+
+void loop() {
+
+    // Update the dummy sensor variable with a random number
+    sensor = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10));
+
+    // print the dummy sensor data
+    std::cout << "Dummy sensor value: " << sensor << std::endl;
+
+    // run at 1 Hz (1 message per second)
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+
+/*
+ * The Arduino header defines app_main and conflicts with having an app_main function
+ * in code. This ifndef makes the code valid in case we remove the Arduino header in
+ * the future.
+ */
+#ifndef Arduino_h
+    extern "C" {
+        void app_main(void);
+    }
+
+    void app_main() {
+        setup();
+        while(1) {
+            loop();
+        }
+    }
+#endif
